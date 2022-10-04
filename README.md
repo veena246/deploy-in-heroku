1f96db032107bd319566cc623fe6d95d25299735
Penugonda Srichand <tp2573@ramco.com>
2022-10-04 18:03:58 -0400

    to deploy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # deploy-in-heroku
+https://api-heroku-flask.herokuapp.com/
+https://test-api-deploy-heroku.herokuapp.com/
--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
+import os
+from flask import send_from_directory
 app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/favicon.png')
 
 
 @app.route('/')
